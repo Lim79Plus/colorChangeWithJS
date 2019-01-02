a80bda5613eda347a77b22bac84c1c1cb5604849
Lim79Plus <masae.h79@gmail.com>
2019-01-02 23:11:31 +0900

    add comment

--- colorChange.js
+++ colorChange.js
@@ -3,10 +3,13 @@ document.getElementById("btnChangeColor").addEventListener("click", changeColor)
 
 var jumbotronColor = document.getElementById("jumbotronColor")
 function changeColor() {
+    
+    //// expected output: a number between 0 and 255
     const r = Math.round(Math.random() * 255);
     const g = Math.round(Math.random() * 255);
     const b = Math.round(Math.random() * 255);
-    
-    jumbotronColor.style.backgroundColor=`rgb(${r},${g},${b})`;
+
+    // the syntactic sugar ${expression}
+    jumbotronColor.style.backgroundColor = `rgb(${r},${g},${b})`;
 
 }
